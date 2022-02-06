5a690e71e2d4cc3616c194ed6e264dac75ed7916
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-06 14:48:06 +0200

    add new structure

--- web/src/pages/Main/index.ts
+++ web/src/pages/Main/index.ts
@@ -1,27 +1,23 @@
 import "./styles.scss"
-
-const toggle = document.getElementById("toggle");
-const theme = window.localStorage.getItem("theme");
-
-if (theme === "dark") document.body.classList.add("dark");
-
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  if (theme === "dark") {
-    window.localStorage.setItem("theme", "light");
-  } else window.localStorage.setItem("theme", "dark");
-});
-
-console.log('main')
+import {onTheme} from "../../logic/header/theme";
 
 const profile = document.getElementById('profile');
 const modalSecurity = document.getElementById('modal-security');
 const closedModal = document.getElementById('closed-modal');
 
-profile.addEventListener('click', ()=> {
+profile.addEventListener('click', () => {
   modalSecurity.style.display = "block";
 });
 
 closedModal.addEventListener('click', () => {
   modalSecurity.style.display = "none";
 });
+
+function init() {
+  onTheme();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  init();
+});
+
