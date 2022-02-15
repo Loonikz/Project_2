e310d1d1ff4501a3cb0cd845dae79fe02e17929a
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-16 00:12:13 +0200

    finish with modal window

---
+++ packages/web/src/logic/header/closedModalWindow.ts
@@ -0,0 +1,4 @@
+export function closedModalWindow(id) {
+  const modal = document.getElementById(id);
+  modal.style.display = 'none';
+}
--- packages/web/src/pages/Main/index.ts
+++ packages/web/src/pages/Main/index.ts
@@ -3,21 +3,24 @@ import { addListener } from '../../logic/header/utils';
 import { changeLng } from '../../logic/header/localization';
 import { changeTheme } from '../../logic/header/theme';
 import { getLocalStorage } from '../../logic/header/getLocalStorage';
+import { changeTabSecurity } from '../../logic/header/changeTabSecurity';
 
-const profile = document.getElementById('profile');
 const modalSecurity = document.getElementById('modal-security');
+const btnProfile = document.getElementById('profile');
 const closedModal = document.getElementById('closed-modal');
 const create = document.getElementById('create');
 const update = document.getElementById('update');
 const modalCreateUpdate = document.getElementById('modal-create-update');
 const closedCreateModal = document.getElementById('closed-create-update');
 
-profile.addEventListener('click', () => {
+btnProfile.addEventListener('click', () => {
   modalSecurity.style.display = 'block';
 });
-
 closedModal.addEventListener('click', () => {
   modalSecurity.style.display = 'none';
+});
+create.addEventListener('click', () => {
+  modalCreateUpdate.style.display = 'block';
 });
 create.addEventListener('click', () => {
   modalCreateUpdate.style.display = 'block';
@@ -32,8 +35,10 @@ closedCreateModal.addEventListener('click', () => {
 function init() {
   addListener('dropdownLanguage', 'change', changeLng);
   addListener('dropdownTheme', 'change', changeTheme);
-
   getLocalStorage();
+  // addListener('profile', 'click', openModalWindow('modal-security'));
+  // addListener('closed-modal', 'click', closedModalWindow('modal-security'));
+  changeTabSecurity();
 }
 
 document.addEventListener('DOMContentLoaded', init);
