da5c5a76e7a23f3cb678bfd0a58e0ccad03e627d
Juliya Likhatskaya <beholderka1111@gmail.com>
2022-02-15 13:26:49 +0200

    feat(web): refactor to eslint

--- packages/web/src/logic/header/theme.ts
+++ packages/web/src/logic/header/theme.ts
@@ -1,13 +1,11 @@
-export function changeTheme(event){
-  const theme = window.localStorage.getItem("theme");
-
+export function changeTheme(event) {
+  const theme = window.localStorage.getItem('theme');
   const selectedLang = event ? event.target.value : 'light';
-
   if (selectedLang === 'dark') {
-    document.body.classList.add("dark");
-    window.localStorage.setItem("theme", "dark");
+    document.body.classList.add('dark');
+    window.localStorage.setItem('theme', 'dark');
   } else {
     document.body.classList.remove('dark');
-    window.localStorage.setItem("theme", "light");
+    window.localStorage.setItem('theme', 'light');
   }
 }
