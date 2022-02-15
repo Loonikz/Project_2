10b0d4b448adb094f7877ffd05d93c9bb10117a3
Nataliia Sychevska <petriwna43@gmail.com>
2022-02-15 17:41:48 +0200

    add modal window create and update

--- packages/web/src/pages/Main/index.ts
+++ packages/web/src/pages/Main/index.ts
@@ -1,12 +1,16 @@
 import './styles.scss';
-import { addListener } from '../../logic/header/utils';
-import { changeLng } from '../../logic/header/localization';
-import { changeTheme } from '../../logic/header/theme';
-import { getLocalStorage } from '../../logic/header/getLocalStorage';
+import {addListener} from '../../logic/header/utils';
+import {changeLng} from '../../logic/header/localization';
+import {changeTheme} from '../../logic/header/theme';
+import {getLocalStorage} from '../../logic/header/getLocalStorage';
 
 const profile = document.getElementById('profile');
 const modalSecurity = document.getElementById('modal-security');
 const closedModal = document.getElementById('closed-modal');
+const create = document.getElementById('create');
+const update = document.getElementById('update');
+const modalCreateUpdate = document.getElementById('modal-create-update');
+const closedCreateModal = document.getElementById('closed-create-update');
 
 profile.addEventListener('click', () => {
   modalSecurity.style.display = 'block';
@@ -14,6 +18,15 @@ profile.addEventListener('click', () => {
 
 closedModal.addEventListener('click', () => {
   modalSecurity.style.display = 'none';
+});
+create.addEventListener('click', () => {
+  modalCreateUpdate.style.display = 'block';
+});
+update.addEventListener('click', () => {
+  modalCreateUpdate.style.display = 'block';
+});
+closedCreateModal.addEventListener('click', () => {
+  modalCreateUpdate.style.display = 'none';
 });
 
 function init() {
