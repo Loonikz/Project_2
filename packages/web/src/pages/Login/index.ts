7296e28b0c8e4a8d60effcb3ca48c1ec833c7118
Juliya Likhatskaya <beholderka1111@gmail.com>
2022-02-17 20:48:25 +0200

    feat(web): refactor login

--- packages/web/src/pages/Login/index.ts
+++ packages/web/src/pages/Login/index.ts
@@ -1,6 +1,6 @@
 import './styles.scss';
 import { changeTheme } from '../../logic/header/theme';
-import { addListener } from '../../logic/header/utils';
+import { addListener, fromLocaleStorageToDropDown } from '../../logic/header/utils';
 import { changeLng } from '../../logic/header/localization';
 import { getLocalStorage } from '../../logic/header/getLocalStorage';
 import { inputLoginValidation, inputPasswordValidation } from './logic';
@@ -10,6 +10,9 @@ function init() {
     url: '/login',
     validateStatus: [false, false],
   };
+  fromLocaleStorageToDropDown('changeTheme', 'theme', ['light', 'dark']);
+  fromLocaleStorageToDropDown('changeLanguage', 'lang', ['en', 'ru']);
+  changeLng();
 
   addListener('login-reg', 'input', inputLoginValidation.bind(null, state));
   addListener('password-reg', 'input', inputPasswordValidation.bind(null, state));
