89d0ed34d59e99a85074f62af2b624fcd6cd1f93
Loonikz <loonik.benitez@gmail.com>
2022-02-17 18:26:28 +0200

    feat(server):add fn logIn, cookie

--- packages/server/src/modules/TokenAcc.ts
+++ packages/server/src/modules/TokenAcc.ts
@@ -1,14 +1,23 @@
-const jwt = require('jsonwebtoken');
-const secret = require('../config/config')
+import { Response } from 'express';
 
-function authToken (req, res, next)  {
-  const token = req.headers.authorization;
+const jwt = require('jsonwebtoken');
+
+export function generateAccessToken(res: Response, id) {
+  const token = jwt.sign({'id': id}, <string>process.env.JWT_SECRET, {expiresIn: "24h"})
+
+  const cookieOptions = {
+    expires: new Date(Date.now() + Number(process.env.JWT_EXPIRES) * 24 * 60 * 60 * 1000),
+    httponly: true,
+  };
+  res.cookie('jwt', token, cookieOptions);
+}
+
+export function authToken (req, res, next)  {
+  const token = req.cookies.jwt;
   if (!token) return res.sendStatus(401).json({message: "Вы не авторизированы"});
-  jwt.verify(token, secret, (err, user) => {
+  jwt.verify(token, <string>process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(401).json({message: "Вы не авторизированы"});
     req.user = user;
     next();
   });
 }
-
-module.exports = authToken;
