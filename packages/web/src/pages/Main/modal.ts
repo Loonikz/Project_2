fb7040e9c0ce6204ca3b03b15743fb89e12d9a59
Juliya Likhatskaya <beholderka1111@gmail.com>
2022-02-19 13:00:45 +0200

    feat (web): add fn close modal

--- packages/web/src/pages/Main/modal.ts
+++ packages/web/src/pages/Main/modal.ts
@@ -9,12 +9,30 @@ export function closeCreateModal() {
   setNodeValue('number');
   setNodeValue('email');
   setNodeValue('company');
-  setInnerText(`first-name-message`,'');
-  setInnerText(`last-name-message`,'');
-  setInnerText(`age-message`,'');
-  setInnerText(`city-message`,'');
-  setInnerText(`number-message`,'');
-  setInnerText(`email-message`,'');
-  setInnerText(`company-message`,'');
-  setStyleDisplay( 'modal-create-update', 'none');
+  setInnerText(`first-name-message`, '');
+  setInnerText(`last-name-message`, '');
+  setInnerText(`age-message`, '');
+  setInnerText(`city-message`, '');
+  setInnerText(`number-message`, '');
+  setInnerText(`email-message`, '');
+  setInnerText(`company-message`, '');
+  setStyleDisplay('modal-create-update', 'none');
+}
+
+export function closeSecurity() {
+  setNodeValue('new-login');
+  setNodeValue('password');
+  setNodeValue('confirm-password');
+  setNodeValue('new-pass');
+  setNodeValue('new-password');
+  setNodeValue('confirm-pass-edit');
+  setInnerText(`changeLogin-message`, '');
+  setInnerText(`password-message`, '');
+  setInnerText(`confirm-password-message`, '');
+  setInnerText(`change-login-message`, '');
+  setInnerText(`new-pass-message`, '');
+  setInnerText(`new-password-message`, '');
+  setInnerText(`confirm-pass-edit-message`, '');
+  setInnerText(`change-password-message`, '');
+  setStyleDisplay('modal-security', 'none');
 }
