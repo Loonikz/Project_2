faf892f127fb977f0642a6d01fc9a305d03cce04
Loonikz <loonik.benitez@gmail.com>
2022-02-20 18:50:07 +0200

    feat(web):get comments for eventClickWithoutModal

--- packages/web/src/logic/header/eventClickWithoutModal.ts
+++ packages/web/src/logic/header/eventClickWithoutModal.ts
@@ -1,12 +1,12 @@
-import { closedModalWindow } from './closedModalWindow';
-
-export function eventClickWithoutModal(className, event) {
-  let targetNode = event.target;
-  while (!targetNode.classList.contains(className) && targetNode.localName !== 'body') {
-    targetNode = targetNode.parentNode;
-  }
-  if (targetNode.classList.contains(className)) {
-    return;
-  }
-  closedModalWindow(targetNode.id);
-}
+// import { closedModalWindow } from './closedModalWindow';
+//
+// export function eventClickWithoutModal(className, event) {
+//   let targetNode = event.target;
+//   while (!targetNode.classList.contains(className) && targetNode.localName !== 'body') {
+//     targetNode = targetNode.parentNode;
+//   }
+//   if (targetNode.classList.contains(className)) {
+//     return;
+//   }
+//   closedModalWindow(targetNode.id);
+// }
